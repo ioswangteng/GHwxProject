5c254fe4173ff3a6ca25910a652dffd3041bac8a
wangteng <wangteng@romens.cn>
2020-03-27 13:36:13 +0800

    注释

--- miniprogram/testprogram/mycharts/mycharts.js
+++ miniprogram/testprogram/mycharts/mycharts.js
@@ -2,7 +2,8 @@ import F2 from '@antv/wx-f2'; // 注：也可以不引入， initChart 方法已
 
 let chart = null;
 
-function initChart(canvas, width, height, F2) { // 使用 F2 绘制图表
+function initChart(canvas, width, height, F2) { // 使用 F2 
+// F2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
   const data = [
     { year: '1951 年', sales: 38 },
     { year: '1952 年', sales: 52 },
@@ -13,12 +14,13 @@ function initChart(canvas, width, height, F2) { // 使用 F2 绘制图表
     { year: '1960 年', sales: 38 },
     { year: '1962 年', sales: 38 },
   ];
+  // Step 1: 创建 Chart 对象
   chart = new F2.Chart({
     el: canvas,
     width,
     height
   });
-
+// Step 2: 载入数据源
   chart.source(data, {
     sales: {
       tickCount: 5
@@ -33,7 +35,9 @@ function initChart(canvas, width, height, F2) { // 使用 F2 绘制图表
       items[0].value = '¥ ' + items[0].value;
     }
   });
+  // Step 3：创建图形语法，绘制柱状图，由 year 和 sales 两个属性决定图形位置，year 映射至 x sales 映射至 y 轴
   chart.interval().position('year*sales');
+  // Step 4: 渲染图表
   chart.render();
   return chart;
 }
